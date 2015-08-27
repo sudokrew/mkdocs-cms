8b5b10e21ccc062c70b95cb331d46d11c24985b8
Derek Ahn <derek@sudokrew.com>
2015-08-27 13:53:26 -1000

    setting up server file

---
+++ app.js
@@ -0,0 +1,54 @@
+var handlebars = require('express-hbs');
+var express    = require('express');
+var app        = express();
+
+require('dotenv').load();
+require('helpers');
+
+/**
+ * View Engine
+ */
+app.engine('hbs', handlebars.express4({
+  defaultLayout: __dirname + '/templates/layout/default.hbs',
+  partialsDir: __dirname + '/templates/partials',
+  layoutDir: __dirname + '/templates/layout',
+  extname: '.hbs'
+}));
+
+
+// Configure views path
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/templates');
+app.set('view engine', 'hbs');
+
+
+/**
+ * Middleware
+ */
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('body-parser').json());
+app.use(require('morgan')('dev'));
+
+
+/**
+ * Routes
+ */
+app.use('/', require('api'));
+app.use('*', require('middleware').err400);
+app.use(require('middleware').err500);
+
+
+/**
+ * MkDocs Process (port: 8000)
+ */
+require('child_process').exec(env.MKDOCS_SERVE, function(err, out) {
+  console.log(err? err : out);
+});
+
+
+/**
+ * Runs Node Server
+ */
+app.listen(Number(env.SERVER_PORT), function() {
+  console.log('Listening on port %d', this.address().port());
+});
